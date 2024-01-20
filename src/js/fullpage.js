018bece776b4401523b70e6aed8b5cdfaca58eed
andrsweb <d3vv.proo@gmail.com>
2024-01-20 16:41:51 +0700

    Another fix

--- src/js/fullpage.js
+++ src/js/fullpage.js
@@ -11,7 +11,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const fullPageSettings = () => {
     const header = document.querySelector('.header')
-    const sections = document.querySelectorAll('.fp-section')
 
     new fullpage('#fullpage', {
         licenseKey: '9DMW8-YL9I9-IINYH-5PGM7-DFOSK',
@@ -23,16 +22,15 @@ const fullPageSettings = () => {
         afterLoad: function (origin, destination, direction) {
 
             if (destination.index === 1) {
-                fullpage_api.setAllowScrolling(false) 
-            } 
-
-            if (destination.index === 2) {
                 fullpage_api.setAllowScrolling(false)
             }
 
-            if(destination.index > 0) {
+            if (destination.index === 2) {
                 header.classList.add('removed')
-            } else header.classList.remove('removed')
+                fullpage_api.setAllowScrolling(false)
+            } else {
+                header.classList.remove('removed')
+            }
         }
     })
 }
@@ -43,5 +41,25 @@ const initSwiper = (selector) => {
         slidesPerView: 1,
         spaceBetween: 20,
         speed: 1200,
+    })
+
+    let isTransitioning = false
+
+    swiper.el.addEventListener('wheel', e => {
+        const direction = e.deltaY > 0 ? 'next' : 'prev'
+
+        if (!isTransitioning) {
+            isTransitioning = true
+
+            if (direction === 'next') {
+                swiper.slideNext()
+            } else {
+                swiper.slidePrev()
+            }
+
+            setTimeout(() => {
+                isTransitioning = false
+            }, 1200)
+        }
     })
 }
