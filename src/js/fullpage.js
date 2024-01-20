ffbce923084d37dcd2b2568dee93dad35fdb552b
andrsweb <d3vv.proo@gmail.com>
2024-01-21 00:08:30 +0700

    last changes

--- src/js/fullpage.js
+++ src/js/fullpage.js
@@ -6,13 +6,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
     fullPageSettings()
 
-    initSwiper('.swiper.slot-swiper')
-    initSwiper('.swiper.nft-swiper')
+    initSlotSwiper('.swiper.slot-swiper')
+    initNftSwiper('.swiper.nft-swiper')
 })
 
 const fullPageSettings = () => {
     const header = document.querySelector('.header')
     const footer = document.querySelector('.footer')
+    let slotSwiperAnimated = false
 
     new fullpage('#fullpage', {
         licenseKey: '9DMW8-YL9I9-IINYH-5PGM7-DFOSK',
@@ -25,7 +26,7 @@ const fullPageSettings = () => {
             if (destination.index === 1) {
                 fullpage_api.setAllowScrolling(false)
                 header.classList.add('removed')
-            } else if(destination.index === 0) {
+            } else if (destination.index === 0) {
                 header.classList.remove('removed')
             }
 
@@ -36,27 +37,85 @@ const fullPageSettings = () => {
             if (destination.index === 3) {
                 footer.classList.add('visible')
             } else footer.classList.remove('visible')
+        },
+
+        onLeave: function (origin, destination, direction) {
+            if (origin.index === 0 || origin.index === 2) {
+                const slotSwiper = document.querySelector('.swiper.slot-swiper')
+                const lastSlide = slotSwiper.querySelector('.swiper-slide:last-child')
+
+                if (lastSlide) {
+                    if (lastSlide.classList.contains('animated')) {
+                        lastSlide.classList.remove('animated')
+                    }
+
+                    if (!lastSlide.classList.contains('closed')) {
+                        lastSlide.classList.add('closed')
+                    }
+
+                    slotSwiperAnimated = false
+                }
+            }
+            fullpage_api.setAllowScrolling(true)
         }
     })
 }
 
-const initSwiper = (selector) => {
+const initNftSwiper = (selector) => {
     const swiper = new Swiper(selector, {
         direction: 'horizontal',
         slidesPerView: 1,
         spaceBetween: 20,
-        speed: 1200,
+        speed: 900,
+    })
+
+    let isTransitioning = false
+
+    swiper.el.addEventListener('wheel', e => {
+        const direction = e.deltaY > 0 ? 'next' : 'prev'
+
+        if (!isTransitioning) {
+            isTransitioning = true
+
+            if (direction === 'next') {
+                swiper.slideNext()
+            } else {
+                swiper.slidePrev()
+            }
+
+            setTimeout(() => isTransitioning = false, 900)
+
+            if ((direction === 'next' && swiper.isEnd) || (direction === 'prev' && swiper.activeIndex === 0)) {
+                setTimeout(() => fullpage_api.setAllowScrolling(true, direction === 'next' ? 'down' : 'up'), 500)
+            } else {
+                fullpage_api.setAllowScrolling(false)
+            }
+        }
+    })
+}
+
+const initSlotSwiper = (selector) => {
+    const swiper = new Swiper(selector, {
+        direction: 'horizontal',
+        slidesPerView: 1,
+        spaceBetween: 20,
+        speed: 900,
 
         on: {
             slideChange: () => {
                 const prevSlide = swiper.slides[swiper.previousIndex]
+                const lastSlide = swiper.slides[swiper.slides.length - 1]
+                const isLastSlideActive = swiper.isEnd && swiper.activeIndex === swiper.slides.length - 1
+
                 if (prevSlide) {
                     prevSlide.classList.remove('animated')
                 }
 
-                const isLastSlide = swiper.isEnd
-                if (isLastSlide) {
-                    swiper.slides[swiper.activeIndex].classList.add('animated')
+                if (isLastSlideActive) {
+                    lastSlide.classList.remove('closed')
+                    lastSlide.classList.add('animated')
+                    fullpage_api.setAllowScrolling(false) // не отрабатывает сука
+                    setTimeout(() => fullpage_api.moveSectionDown(), 7500) 
                 }
             }
         }
@@ -76,9 +135,17 @@ const initSwiper = (selector) => {
                 swiper.slidePrev()
             }
 
-            setTimeout(() => isTransitioning = false, 1200)
+            setTimeout(() => isTransitioning = false, 900)
 
-            setTimeout(() => fullpage_api.setAllowScrolling(true), 2000)
+            if ((direction === 'next' && swiper.isEnd) || (direction === 'prev' && swiper.activeIndex === 0)) {
+                setTimeout(() => fullpage_api.setAllowScrolling(true, direction === 'next' ? 'down' : 'up'), 900)
+            } else {
+                fullpage_api.setAllowScrolling(false)
+            }
+
+            if(direction === 'next' && swiper.isEnd || direction === 'prev' && swiper.isEnd) {
+                setTimeout(() => fullpage_api.setAllowScrolling(true, direction === 'next' ? 'down' : 'up'), 6000)
+            }
         }
     })
 }
