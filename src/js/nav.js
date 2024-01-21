42b5cd746db1437d7dae26294d5875d774d564f4
andrsweb <d3vv.proo@gmail.com>
2024-01-21 15:30:12 +0700

    created hover on menu

--- src/js/nav.js
+++ src/js/nav.js
@@ -4,4 +4,33 @@ document.addEventListener('DOMContentLoaded', () => {
     followCursor()
 })
 
-const followCursor = () => {}
+const followCursor = () => {
+    const marker = document.getElementById('marker')
+    const current = document.querySelector('.current')
+    const listItems = document.querySelectorAll('.menu-item')
+    const ul = document.querySelector('.header__nav')
+
+    marker.style.bottom = `-${marker.offsetHeight / 2}px`
+    marker.style.left = `${current.offsetLeft}px`
+    marker.style.width = `${current.offsetWidth}px`
+    marker.style.display = 'block'
+
+    listItems.forEach(item => {
+        item.addEventListener('mouseover', () => {
+            const offsetLeft = item.offsetLeft
+            const width = item.offsetWidth || current.offsetWidth
+            const left = offsetLeft === 0 ? 0 : offsetLeft || current.offsetLeft
+            marker.style.left = `${left}px`
+            marker.style.width = `${width}px`
+        })
+    })
+
+    ul.addEventListener('mouseleave', () => {
+        marker.style.left = `${current.offsetLeft}px`
+        marker.style.width = `${current.offsetWidth}px`
+    })
+}
+
+
+
+
